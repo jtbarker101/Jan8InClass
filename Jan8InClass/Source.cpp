4f55fa689faee911da6ee737d507473c6a952aee
Jon Barker <jtbarker50@gmail.com>
2020-01-08 14:56:03 -0700

    second commit

---
+++ Jan8InClass/Source.cpp
@@ -0,0 +1,16 @@
+//Jon Barker
+//This is my own work
+//In Class January 8
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int w;
+	cout << "Please enter the weight you want to convert. ";
+	cin >> w;
+	cout << w << " in kilograms is " << w * 0.454;
+
+	return 0;
+}
